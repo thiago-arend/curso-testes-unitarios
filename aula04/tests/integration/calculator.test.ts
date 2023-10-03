7f5d65f457faf9bade32be65ac73e54eb591443c
thiago-arend <thiago.arend@gmail.com>
2023-10-02 23:08:22 -0300

    exercicio 04

--- aula04/tests/integration/calculator.test.ts
+++ aula04/tests/integration/calculator.test.ts
@@ -36,51 +36,5 @@ describe("calculator tests", () => {
     expect(status).toBe(200);
     expect(body.result).toBe(4);
   });
-
-  it("should subtract two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "sub",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
-  });
-  it("should multiply two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "mul",
-      n1: 3,
-      n2: 3
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(9);
-  });
-
-  it("should divide two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(1);
-  });
-
-  it("should return 0 when diving by zero", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 0
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
-  });
+  
 });
--- aula04/tests/unit/calculator.test.ts
+++ aula04/tests/unit/calculator.test.ts
@@ -1,5 +1,28 @@
+import calculator from "calculator";
+
 describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+  it("should sum two numbers", async () => {
+    const result = calculator.sum(2, 2);
+    expect(result).toBe(4);
+  });
+
+  it("should subtract two numbers", async () => {
+    const result = calculator.sub(2, 2);
+    expect(result).toBe(0);
+  });
+
+  it("should multiply two numbers", async () => {
+    const result = calculator.mul(3, 3);
+    expect(result).toBe(9);
+  });
+
+  it("should divide two numbers", async () => {
+    const result = calculator.div(3, 3);
+    expect(result).toBe(1);
+  });
+
+  it("should return 0 when diving by zero", async () => {
+    const result = calculator.div(3, 0);
+    expect(result).toBe(0);
   });
 })
