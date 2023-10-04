7c6ecf82e8d3690369fe3a4801618589170518e7
thiago-arend <thiago.arend@gmail.com>
2023-10-04 02:35:31 -0300

    exercicio 05

--- aula05/tests/unit/pacientes.test.ts
+++ aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,17 @@
+import { faker } from "@faker-js/faker";
+import { generateProtocolForPacient } from "protocols-generator";
+
+jest.mock("uuid", () => {
+  return {
+    v4: () => { return "valor simulado no mock!" }
+  }
+});
+
 describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+  it("should generate a protocol for pacient", () => {
+    const protocol = generateProtocolForPacient(faker.person.firstName(), faker.person.lastName(), faker.datatype.boolean());
+    expect(protocol).toEqual({
+      ...protocol
+    });
   });
 });
