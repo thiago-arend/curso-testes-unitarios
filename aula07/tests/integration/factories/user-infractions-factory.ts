0adafe898bc056078826ce8144197b3d72c88c20
thiago-arend <thiago.arend@gmail.com>
2023-10-04 17:22:03 -0300

    exercicio 07

--- aula07/tests/integration/factories/user-infractions-factory.ts
+++ aula07/tests/integration/factories/user-infractions-factory.ts
@@ -26,6 +26,34 @@ export async function generateUserWithNInfractions(nInfractions = 1) {
   return user;
 }
 
+export function generateUserMock() {
+  const user = {
+    id: faker.number.int(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    licenseId: faker.internet.ipv4().replace("/./g", "")
+  }
+
+  return user;
+}
+
+export function generateInfractionsForUserMock(nInfractions: number, userId: number) {
+  const infraactions = [];
+  for (let i = 0; i < nInfractions; i++) {
+    const infraction = {
+      id: i + 1,
+      userId,
+      date: new Date(),
+      cost: faker.number.int({ min: 100, max: 1000 }),
+      level: getLevel(),
+      description: faker.company.catchPhrase()
+    }
+    infraactions.push(infraction);
+  }
+
+  return infraactions;
+}
+
 function getLevel(): Level {
   const levels = ["LIGHT", "MEDIUM", "SEVERE", "VERY_SEVERE"];
   const random = Math.floor(Math.random() * levels.length);
--- aula07/tests/unit/infractions.test.ts
+++ aula07/tests/unit/infractions.test.ts
@@ -1,11 +1,38 @@
+import { generateInfractionsForUserMock, generateUserMock, generateUserWithNInfractions } from "../integration/factories/user-infractions-factory";
+import * as infractionsRepository from "../../src/infractions-repository";
+import * as usersRepository from "../../src/users-repository";
+import * as infractionsService from "../../src/infractions-service";
+
 describe("Infractions Service Tests", () => {
   it("should get infractions from user", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const userMock = generateUserMock();
+    const userInfracMoc = generateInfractionsForUserMock(3, userMock.id);
+
+    jest.spyOn(usersRepository, "getUserByDocument").mockImplementationOnce((): any => {
+      return userMock
+    });
+
+    jest.spyOn(infractionsRepository, "getInfractionsFrom").mockImplementationOnce((): any => {
+      return userInfracMoc
+    });
+
+    const userInfractions = await infractionsService.getInfractionsFrom(userMock.licenseId);
+
+    expect(infractionsRepository.getInfractionsFrom).toBeCalledWith(userMock.id);
+    expect(userInfractions).toEqual({ ...userMock, infractions: userInfracMoc });
   });
 
-  it("should throw an error when driver license does not exists", () => {
-    // TODO
-    expect(true).toBe(true);
-  })
+  it("should throw an error when driver license does not exists", async () => {
+    const licenceIdMock = "doesn't matter";
+
+    jest.spyOn(usersRepository, "getUserByDocument").mockImplementationOnce((): any => {
+      return null // user not found
+    });
+
+    const promiseToBeRejected = infractionsService.getInfractionsFrom(licenceIdMock);
+
+    expect(usersRepository.getUserByDocument).toBeCalledWith(licenceIdMock);
+    expect(promiseToBeRejected).rejects.toEqual({ type: "NOT_FOUND", message: "Driver not found." });
+  });
+
 });
