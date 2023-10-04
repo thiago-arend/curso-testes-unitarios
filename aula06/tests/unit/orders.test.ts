8e77ecb368c21b5b55cf6e0b817463a5f9286ae9
thiago-arend <thiago.arend@gmail.com>
2023-10-04 15:25:40 -0300

    exercicio 06

--- aula06/tests/unit/orders.test.ts
+++ aula06/tests/unit/orders.test.ts
@@ -1,22 +1,68 @@
-import { faker } from "@faker-js/faker";
-
-import { createOrder, getOrderByProtocol } from "../../src/order-service";
+import * as orderService from "../../src/order-service";
 import * as orderRepository from "../../src/order-repository";
 import { OrderInput } from "../../src/validator";
 
+type OrderMockData = {
+  protocol: string,
+  status: string
+}
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const order: OrderInput = {
+      client: "João",
+      description: "Pedido João"
+    };
+
+    jest.spyOn(orderRepository, "create").mockImplementationOnce((): any => {
+      return {
+        protocol: new Date().getTime().toString(),
+        status: "IN_PREPARATION"
+      }
+    });
+
+    const createdOrder = await orderService.createOrder(order);
+
+    expect(orderRepository.create).toBeCalledWith(order);
+    expect(createdOrder).toEqual({
+      protocol: expect.any(String),
+      status: "IN_PREPARATION"
+    });
   });
 
   it("should return an order based on the protocol", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const mockData: OrderMockData = {
+      protocol: new Date().getTime().toString(),
+      status: "IN_PREPARATION"
+    }
+
+    jest.spyOn(orderRepository, "getByProtocol").mockImplementationOnce((): any => {
+      return mockData
+    });
+
+    const returnedOrder = await orderService.getOrderByProtocol(mockData.protocol);
+
+    expect(orderRepository.getByProtocol).toBeCalledWith(mockData.protocol);
+    expect(returnedOrder).toEqual(mockData);
   });
 
   it("should return status INVALID when protocol doesn't exists", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const invalidProtocol = new Date("2025-01-01").getTime().toString();
+
+    jest.spyOn(orderRepository, "getByProtocol").mockImplementationOnce((): any => {
+      return undefined
+    });
+
+    const returnedOrder = await orderService.getOrderByProtocol(invalidProtocol);
+
+    expect(orderRepository.getByProtocol).toBeCalledWith(invalidProtocol);
+    expect(returnedOrder).toEqual({
+      protocol: invalidProtocol,
+      status: "INVALID"
+    });
   });
 });
